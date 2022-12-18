f8c42cb14998adaa304376af962e8bd52468b353
Jovan <jovanlemuel9103@gmail.com>
2022-12-18 16:52:23 +0700

    Jevon Ganteng 11

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "description",
+        "image_name"
+    ];
+
+    private static $products = [
+        [
+            'id' => 1,
+            'name' => 'Product 1',
+            'description' => 'Product 1 description',
+            'image_name' => 'Product1.jpg',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Product 2',
+            'description' => 'Product 2 description',
+            'image_name' => 'Product2.jpg',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Product 3',
+            'description' => 'Product 3 description',
+            'image_name' => 'Product3.jpg',
+        ],
+    ];
 }
